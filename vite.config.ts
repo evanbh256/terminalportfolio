9edf5080450871456f305fa2b387b5e3286063dd
Evan Bhandari <75253804+evanbh256@users.noreply.github.com>
2025-11-18 13:39:17 -0600

    minor change

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "node:path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/terminalportfolio/",  // <- repo name
+  base: "/",  // <- repo name
   resolve: {
     alias: { "@": path.resolve(__dirname, "src") }, // optional, helps with "@/..."
   },
